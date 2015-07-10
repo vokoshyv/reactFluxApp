26883be5edf0f0aca48904a6244c991ffdd62e50
vokoshyv <ronsuij@gmail.com>
2015-07-09 23:12:03 -0700

    made small change in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,20 @@ var browserify = require('browserify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
 
+
+var paths = {
+  main: 'src/js/main.js'
+}
+
+gulp.task('browserify', function(){
+  browserify(main)
+    .transform('reactify')
+    .bundle()
+    .pipe(source('main.js'))
+    .pipe(gulp.dest('dist/js'));
+})
+
+gulp.task('copy', function(){
+  gulp.src('src/index.html')
+    .pipe(gulp.desk('dist'));
+})
